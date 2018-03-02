3c8db8bc0df64faf4311706a5aac6b06b110250d
Urban Dzindzinovic <dzindzinovic@yahoo.com>
2018-03-02 12:04:35 +0100

    added tasks.js

---
+++ tasks.js
@@ -0,0 +1,249 @@
+var tasks = [
+    //GROOM - clean:
+    {
+        "label": "grooming",
+        "text": "shower",
+        "priorityFactor": 1
+    },
+    {
+        "label": "grooming",
+        "text": "shave",
+        "priorityFactor": 1
+    },
+    {
+        "label": "grooming",
+        "text": "clip nails",
+        "priorityFactor": 1
+    },
+    {
+        "label": "grooming",
+        "text": "brush teeth",
+        "priorityFactor": 1
+    },
+    {
+        "label": "grooming",
+        "text": "floss",
+        "priorityFactor": 1
+    },
+    //CHORES - clean:
+    {
+        "label": "chores",
+        "text": "clothes, laundry",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "dishes",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "trash can",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "table",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "counter",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "floor",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "sink",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "bathtub",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "tiles on walls",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "light switches",
+        "priorityFactor": 1
+    },
+    {
+        "label": "chores",
+        "text": "windows",
+        "priorityFactor": 1
+    },
+    //BODY - exercise:
+    {
+        "label": "body",
+        "text": "rehabilitation",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "climbing",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "walks",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "hiking",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "handstand",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "planche",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "meditation",
+        "priorityFactor": 1
+    },
+    {
+        "label": "body",
+        "text": "martial arts",
+        "priorityFactor": 1
+    },
+    //MIND - progress, be productive, learn, build:
+    {
+        "label": "mind",
+        "text": "read",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "tutorials",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "programming",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "web site - personal growth",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "calisthenics park",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "mahjong",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "go",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "shogi",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "pencil drawing",
+        "priorityFactor": 1
+    },
+    {
+        "label": "mind",
+        "text": "story writing",
+        "priorityFactor": 1
+    },
+    //RELAX / ENJOY - relax, enjoy:
+    {
+        "label": "relax / enjoy",
+        "text": "salsa",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "movies",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "tv series",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "games",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "read fantasy novels",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "anime",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "manga",
+        "priorityFactor": 1
+    },
+    //vacation:
+    {
+        "label": "relax / enjoy",
+        "text": "mountain biking",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "windsurfing",
+        "priorityFactor": 1
+    },
+    {
+        "label": "relax / enjoy",
+        "text": "snowboarding",
+        "priorityFactor": 1
+    },
+    //COOK / EAT - food:
+    {
+        "label": "cook / eat",
+        "text": "many meals",
+        "priorityFactor": 1
+    },
+    {
+        "label": "cook / eat",
+        "text": "lots of water",
+        "priorityFactor": 1
+    },
+    {
+        "label": "cook / eat",
+        "text": "fruits",
+        "priorityFactor": 1
+    },
+    {
+        "label": "cook / eat",
+        "text": "vegetables",
+        "priorityFactor": 1
+    }
+];
